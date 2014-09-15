2592f3e05334be3275e70f20e4fda6716661fb3f
linoox <pd2614g@gmail.com>
2014-09-15 03:26:56 -0400

    Evo-2.Log product and stock info.Must run older version of Aplication or the evolution and then run this.

--- warehouse/app/controllers/Application.java
+++ warehouse/app/controllers/Application.java
@@ -3,6 +3,8 @@ package controllers;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.avaje.ebean.Ebean;
+
 import models.Product;
 import models.StockItem;
 import models.Warehouse;
@@ -14,29 +16,11 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        Warehouse warehouse = new Warehouse();
-        warehouse.name = "My warehouse";
-        
-        Product product = new Product();
-        product.name = "Stainless steel paperclips, small, 1000pcs";
-        product.ean = 1234L;
-        product.description = "1000 blue paperclips";
-        
-        StockItem item = new StockItem();
-        item.quantity = 15L;
-        item.product = product;
-        
-        item.warehouse = warehouse;
-        warehouse.stock.add(item);
-        
-        product.save();
-        item.save();
-        Logger.info("hope we saved product & item");
-//        List<String> output = new LinkedList<String>();
-//        output.add(String.format("My warehouse is called '%s'",warehouse));
-//        output.add(String.format("It contains %d items",warehouse.stock.size()));
-//        output.add(String.format("The first is: %s",warehouse.stock.get(0)));
-//        
+
+    	Product product = Product.find().byId(1L);
+    	Logger.info(String.format("Product name: %s", product.name));
+    	Logger.info(String.format("StockItems: %d",
+    	product.stockItems.size()));
         
         return ok(index.render("your new application is ready."));
     }
